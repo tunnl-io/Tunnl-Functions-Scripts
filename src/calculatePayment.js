4c13f0076f055f6e73d4ba2f9336333200d90244
Morgan Kuphal <87319522+KuphJr@users.noreply.github.com>
2025-03-17 17:38:02 -0500

    removed comments

--- src/calculatePayment.js
+++ src/calculatePayment.js
@@ -2,7 +2,6 @@ const offerId = bytesArgs[0]
 const requiredPostLiveDurationSeconds = BigInt(bytesArgs[1])
 const maxCreatorPayment = BigInt(bytesArgs[2])
 
-// Fetch private offer data from backend
 const backendRes = await Functions.makeHttpRequest({
   url: `${secrets.backendUrl}/fetch-offer-for-chainlink-function`,
   method: 'POST',
@@ -24,25 +23,6 @@ if (!offerData) {
   throw Error(`No offer data found`)
 }
 
-const startDateSeconds = Math.floor(new Date(offerData.post_submission_start_date).getTime() / 1000)
-const endDateSeconds = Math.floor(new Date(offerData.post_submission_end_date).getTime() / 1000)
-
-// Verify the integrity of the offer data by ensuring the private data SHA256 hash matches the offerId
-// UNUSED FOR NOW
-// const offerDataToHash = {
-//   salt: offerData.salt,
-//   creator_twitter_id: offerData.creator_twitter_id,
-//   required_likes: offerData.required_likes,
-//   sponsorship_criteria: offerData.requirements,
-//   start_date_seconds: startDateSeconds,
-//   end_date_seconds: endDateSeconds,
-// }
-// const offerDataHash = await sha256(JSON.stringify(offerDataToHash))
-// if (`0x${offerDataHash}` !== offerId) {
-//   throw Error(`Offer data hash mismatch 0x${offerDataHash} !== ${offerId}`)
-// }
-
-// We will need to refetch the tweet one final time before payout to confirm it is still live.
 const tweetRes = await Functions.makeHttpRequest({
   url: `${secrets.backendUrl}/fetch-tweet`,
   method: 'POST',
@@ -69,24 +49,10 @@ if (!tweetRes.data.data) {
 }
 const tweetData = tweetRes.data.data
 
-// The post must be live for the required duration
 const postDateSeconds = BigInt(Math.floor(Date.parse(tweetData.created_at) / 1000))
 const postLiveDurationSeconds = BigInt(Math.floor(Date.now() / 1000)) - postDateSeconds
 if (postLiveDurationSeconds < requiredPostLiveDurationSeconds) {
   throw Error(`Tweet was not live long enough`)
 }
 
-// For the mainnet beta, we are not calculating payment based on any metrics (likes, views, etc.)
-// Future Improvement: variable payment amount based on metrics
 return Functions.encodeUint256(maxCreatorPayment)
-
-// Library functions
-// UNUSED FOR NOW
-// async function sha256(text) {
-//   const encoder = new TextEncoder()
-//   const data = encoder.encode(text)
-//   const hashBuffer = await crypto.subtle.digest("SHA-256", data)
-//   const hashArray = Array.from(new Uint8Array(hashBuffer))
-//   const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
-//   return hashHex
-// }
--- src/verifyTweet.js
+++ src/verifyTweet.js
@@ -2,7 +2,6 @@ const offerId = bytesArgs[0]
 const requiredPostLiveDurationSeconds = BigInt(bytesArgs[1])
 const advertiserAddress = bytesArgs[3]
 
-// Fetch private offer data from backend
 const backendRes = await Functions.makeHttpRequest({
   url: `${secrets.backendUrl}/fetch-offer-for-chainlink-function`,
   method: 'POST',
@@ -23,24 +22,6 @@ const offerData = backendRes.data
 if (!offerData) {
   throw Error(`No offer data found`)
 }
-
-const startDateSeconds = Math.floor(new Date(offerData.post_submission_start_date).getTime() / 1000)
-const endDateSeconds = Math.floor(new Date(offerData.post_submission_end_date).getTime() / 1000)
-
-// Verify the integrity of the offer data by ensuring the private data SHA256 hash matches the offerId
-// UNUSED FOR NOW
-// const offerDataToHash = {
-//   salt: offerData.salt,
-//   creator_twitter_id: offerData.creator_twitter_id,
-//   required_likes: offerData.required_likes,
-//   sponsorship_criteria: offerData.requirements,
-//   start_date_seconds: startDateSeconds,
-//   end_date_seconds: endDateSeconds,
-// }
-// const offerDataHash = await sha256(JSON.stringify(offerDataToHash))
-// if (`0x${offerDataHash}` !== offerId) {
-//   throw Error(`Offer data hash mismatch 0x${offerDataHash} !== ${offerId}`)
-// }
 
 const tweetRes = await Functions.makeHttpRequest({
   url: `${secrets.backendUrl}/fetch-tweet`,
@@ -80,12 +61,11 @@ if (!tweetData.created_at) {
   throw Error(`Tweet has no creation date`)
 }
 
-// Ensure the post was made between the offer start and end dates
 const postDateSeconds = BigInt(Math.floor(Date.parse(tweetData.created_at) / 1000))
 if (postDateSeconds < offerData.startDateSeconds || postDateSeconds > offerData.endDateSeconds) {
   throw Error(`Tweet was posted outside the offer window`)
 }
-// Ensure the tweet is at least an hour old so it cannot be edited
+
 if (postDateSeconds > BigInt(Math.floor(Date.now() / 1000)) - BigInt(60 * 60)) {
   throw Error(`Tweet was posted less than 1 hour ago`)
 }
@@ -93,22 +73,10 @@ if (postDateSeconds > BigInt(Math.floor(Date.now() / 1000)) - BigInt(60 * 60)) {
 if (!tweetData.edit_history_tweet_ids) {
   throw Error(`Missing tweet edit history`)
 }
-// Ensure the tweet has not been edited
+
 if (tweetData.edit_history_tweet_ids.length > 1) {
   throw Error(`Tweet has been edited`)
 }
 
-// We don't need to perform the AI verification as the backend will have already done this before calling submitTweet
 const payoutDateSeconds = postDateSeconds + requiredPostLiveDurationSeconds
 return Functions.encodeUint256(payoutDateSeconds)
-
-// Library functions
-// UNUSED FOR NOW
-// async function sha256(text) {
-//   const encoder = new TextEncoder()
-//   const data = encoder.encode(text)
-//   const hashBuffer = await crypto.subtle.digest("SHA-256", data)
-//   const hashArray = Array.from(new Uint8Array(hashBuffer))
-//   const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
-//   return hashHex
-// }
